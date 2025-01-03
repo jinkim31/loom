9700f887c2ee6c9a40d6c9df2bbd66a71a405d7e
jinkim31 <jinkim@kaist.ac.kr>
2025-01-03 19:20:45 +0900

    make server client member function version

--- loom/thread.h
+++ loom/thread.h
@@ -32,8 +32,14 @@ protected:
     template<typename ArgType, typename RetType>
     typename Server<ArgType, RetType>::SharedPtr makeServer(const std::function<RetType(const ArgType&)>& callback);
 
+    template<typename ArgType, typename RetType, typename ThreadObjectType>
+    typename Server<ArgType, RetType>::SharedPtr makeServer(ThreadObjectType* object, RetType (ThreadObjectType::*callbackPtr)(const ArgType&));
+
     template<typename ArgType, typename RetType>
     typename Client<ArgType, RetType>::SharedPtr makeClient(const std::function<void (const RetType&)>& callback=[](const RetType&){});
+
+    template<typename ArgType, typename RetType, typename ThreadObjectType>
+    typename Client<ArgType, RetType>::SharedPtr makeClient(ThreadObjectType* object, void (ThreadObjectType::*callbackPtr)(const RetType&));
 
 
     virtual void step(){}
@@ -84,6 +90,18 @@ typename Server<ArgType, RetType>::SharedPtr loom::Thread::makeServer(const std:
             std::move(transmitter));
 }
 
+template<typename ArgType, typename RetType, typename ThreadObjectType>
+typename Server<ArgType, RetType>::SharedPtr loom::Thread::makeServer(ThreadObjectType* object, RetType (ThreadObjectType::*callbackPtr)(const ArgType&))
+{
+    auto transmitter = makeTransmitter<RetType>();
+    auto receiver = makeReceiver<ArgType>([=](const ArgType& arg){
+        transmitter->transmit((object->*callbackPtr)(arg));
+    });
+    return std::make_shared<Server<ArgType, RetType>>(
+            std::move(receiver),
+            std::move(transmitter));
+}
+
 template<typename ArgType, typename RetType>
 typename Client<ArgType, RetType>::SharedPtr loom::Thread::makeClient(const std::function<void(const RetType &)> &callback)
 {
@@ -93,6 +111,19 @@ typename Client<ArgType, RetType>::SharedPtr loom::Thread::makeClient(const std:
             std::move(transmitter),
             std::move(receiver));
 }
+
+template<typename ArgType, typename RetType, typename ThreadObjectType>
+typename Client<ArgType, RetType>::SharedPtr loom::Thread::makeClient(ThreadObjectType* object, void (ThreadObjectType::*callbackPtr)(const RetType&))
+{
+    auto transmitter = makeTransmitter<ArgType>();
+    auto receiver = makeReceiver<RetType>([=](const RetType& arg){(object->*callbackPtr)(arg);});
+    return std::make_shared<Client<ArgType, RetType>>(
+            std::move(transmitter),
+                    std::move(receiver));
+}
+
+
+
 
 }
 
--- tests/server_client_async.cpp
+++ tests/server_client_async.cpp
@@ -7,15 +7,21 @@ class ProducerThread : public loom::Thread
 public:
     ProducerThread() : Thread("Server", 100)
     {
+        /* lambda version
         server = makeServer<int, double>([](const int& arg){
             std::cout<<"server received arg "<<arg<<std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
             return 3.14 * arg;
         });
+         */
+        server = makeServer<int, double>(this, &ProducerThread::callback);
     }
     loom::Server<int, double>::SharedPtr server;
 private:
-
+    double callback(const int& arg)
+    {
+        return 3.14 * arg;
+    }
 };
 
 class ConsumerThread : public loom::Thread
@@ -23,9 +29,12 @@ class ConsumerThread : public loom::Thread
 public:
     ConsumerThread() : Thread("Client", 100)
     {
+        /* lambda version
         client = makeClient<int, double>([](const double& ret){
             std::cout<<"client received ret: "<<ret<<std::endl;
         });
+         */
+        client = makeClient<int, double>(this, &ConsumerThread::callback);
     }
     loom::Client<int, double>::SharedPtr client;
 protected:
@@ -34,6 +43,10 @@ protected:
         client->requestAsync(count++);
     }
 private:
+    void callback(const double& ret)
+    {
+        std::cout<<"client received ret: "<<ret<<std::endl;
+    }
     int count = 0;
 };
 
